25cd06f48aa8e556d61e89026b899b5c03655081
Jessica Menezes <jessicamenezes833@gmail.com>
2023-06-07 17:50:03 -0300

    Update pages

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// const modalEle = document.querySelector(".modal");
+// console.log(modalEle)
+// const modalImage = document.querySelector(".modalImage");
+// Array.from(document.querySelectorAll(".img-modal")).forEach(item => {
+//   item.addEventListener("click", event => {
+//     modalEle.style.display = "block";
+//     modalImage.src = event.target.src;
+//   });
+// });
+// document.querySelector(".close").addEventListener("click", () => {
+//   modalEle.style.display = "none";
+// });
+
+const alo = document.getElementById('modal');
+console.log(document.getElementById('logo'))
+
+const seeAll = document.querySelector('.ver-mais');
+console.log(seeAll)
