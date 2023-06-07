6c69afb807d0005ca4cb613d0e84bc8f2b89a52e
Jessica Menezes <jessicamenezes833@gmail.com>
2023-06-07 19:28:32 -0300

    Remove comments

--- js/script-inseg.js
+++ js/script-inseg.js
@@ -1,18 +1,3 @@
-const spanSeeAll = document.querySelectorAll('.toggle');
-const btnSeeAll = document.getElementById('ver-mais');
-
-function showAll() {
-  spanSeeAll.forEach(item => item.classList.toggle('hidden'));
-  if (btnSeeAll.textContent === 'VER MENOS') {
-    return btnSeeAll.textContent = 'VER MAIS'
-  } else {
-    return btnSeeAll.textContent = 'VER MENOS'
-  }
-}
-
-btnSeeAll.addEventListener('click', showAll());
-
-
 const images = document.querySelectorAll(".img-modal");
 let imgSrc;
 console.log(images)
